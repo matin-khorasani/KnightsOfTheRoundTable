d8be5c8f362957ef6ee47ea69120089c0c5d2417
matin khorasani <matin.khorasani.p@gmail.com>
2026-05-24 19:38:58 +0330

    Knight class completed (Damage and Heal methods)

--- src/Model/Knight.java
+++ src/Model/Knight.java
@@ -1,28 +1,38 @@
 package Model;
 
+import Model.Enums.KnightClass;
 import Model.Enums.KnightName;
+import Model.Enums.SkillName;
 
 public class Knight {
     private final KnightName knightName;
     private int hp;
     private int ap;
-    private int currentAttack;
-    private int currentMagicAttack;
-    private int currentDefense;
-    private int currentSpeed;
     private boolean isStunned;
     private boolean isAlive;
+    private int damageDealt;
+    private final int baseAttack;
+    private final int baseMagicAttack;
+    private final int baseDefense;
+    private final int baseSpeed;
+    private int attackPercent = 100;
+    private int magicAttackPercent = 100;
+    private int defensePercent = 100;
+    private int speedPercent = 100;
+
 
     public Knight(KnightName knightName) {
         this.knightName = knightName;
         this.hp = knightName.getMaxHp();
         this.ap = 3;
-        this.currentAttack = knightName.getAttack();
-        this.currentDefense = knightName.getDefense();
-        this.currentMagicAttack = knightName.getMagicAttack();
-        this.currentSpeed = knightName.getSpeed();
         this.isStunned = false;
         this.isAlive = true;
+        this.damageDealt = 0;
+        this.baseAttack = knightName.getAttack();
+        this.baseMagicAttack = knightName.getMagicAttack();
+        this.baseDefense = knightName.getDefense();
+        this.baseSpeed = knightName.getSpeed();
+
     }
     public void setHp(int hp) {
         this.hp = Math.max(0, Math.min(hp, knightName.getMaxHp()));
@@ -45,22 +55,6 @@ public class Knight {
         return ap;
     }
 
-    public int getCurrentAttack() {
-        return currentAttack;
-    }
-
-    public int getCurrentMagicAttack() {
-        return currentMagicAttack;
-    }
-
-    public int getCurrentDefense() {
-        return currentDefense;
-    }
-
-    public int getCurrentSpeed() {
-        return currentSpeed;
-    }
-
     public boolean isStunned() {
         return isStunned;
     }
@@ -73,20 +67,113 @@ public class Knight {
         isStunned = stunned;
     }
 
-    public void setCurrentSpeed(int currentSpeed) {
-        this.currentSpeed = currentSpeed;
+
+    public int getDamageDealt() {
+        return damageDealt;
+    }
+    public int getCurrentAttack() {
+        return (baseAttack * attackPercent) / 100;
     }
 
-    public void setCurrentDefense(int currentDefense) {
-        this.currentDefense = currentDefense;
+    public int getCurrentMagicAttack() {
+        return (baseMagicAttack * magicAttackPercent) / 100;
     }
 
-    public void setCurrentMagicAttack(int currentMagicAttack) {
-        this.currentMagicAttack = currentMagicAttack;
+    public int getCurrentDefense() {
+        return (baseDefense * defensePercent) / 100;
     }
 
-    public void setCurrentAttack(int currentAttack) {
-        this.currentAttack = currentAttack;
+    public int getCurrentSpeed() {
+        return (baseSpeed * speedPercent) / 100;
     }
 
+    public int getAttackPercent() {
+        return attackPercent;
+    }
+
+    public int getMagicAttackPercent() {
+        return magicAttackPercent;
+    }
+
+    public int getDefensePercent() {
+        return defensePercent;
+    }
+
+    public int getSpeedPercent() {
+        return speedPercent;
+    }
+
+    public void setDamageDealt(int damageDealt) {
+        this.damageDealt = damageDealt;
+    }
+    public void setAttackPercent(int percent) {
+        this.attackPercent = percent;
+    }
+
+    public void setMagicAttackPercent(int percent) {
+        this.magicAttackPercent = percent;
+    }
+
+    public void setDefensePercent(int percent) {
+        this.defensePercent = percent;
+    }
+
+    public void setSpeedPercent(int percent) {
+        this.speedPercent = percent;
+    }
+
+    public int calculateDamage(Knight target, SkillName skillName) {
+        int damage;
+        if (getKnightName().getKnightClass() == KnightClass.MAGE) {
+            damage = getCurrentMagicAttack();
+        }
+        else if (skillName == SkillName.HEAVY_STRIKE) {
+            damage = (int) Math.floor(1.5 * getCurrentAttack()) - (int) Math.floor(0.3 * target.getCurrentDefense());
+        }
+        else {
+            damage = getCurrentAttack() - (int) Math.floor(0.3 * target.getCurrentDefense());
+        }
+        return damage;
+    }
+    public void applyDamage(int damage, Knight attacker) {
+        this.setHp(this.getHp() - damage);
+        attacker.setDamageDealt(attacker.getDamageDealt() + damage);
+    }
+    public boolean dodgeStatus(Knight attacker) {
+        KnightClass attackerClass = attacker.getKnightName().getKnightClass();
+        if (attackerClass == KnightClass.COMMANDER || attackerClass == KnightClass.WARRIOR) {
+            return attacker.getCurrentAttack() < this.getCurrentSpeed();
+        } else if (attackerClass == KnightClass.MAGE || attackerClass == KnightClass.HEALER) {
+            return attacker.getCurrentMagicAttack() < this.getCurrentSpeed();
+        }
+        return false;
+    }
+    public void resetToBaseStats() {
+        this.attackPercent = 100;
+        this.magicAttackPercent = 100;
+        this.defensePercent = 100;
+        this.speedPercent = 100;
+    }
+    public int applyHeal(int percent, boolean isRevive) {
+        int maxHp = knightName.getMaxHp();
+        int currentHp = this.hp;
+        int healAmount = (maxHp * Math.abs(percent)) / 100;
+        if (isRevive) {
+            if (this.isAlive) {
+                return -1;
+            }
+            this.hp = Math.min(healAmount, maxHp);
+            this.ap = 3;
+            this.isAlive = true;
+            return healAmount;
+        }
+        if (percent < 0) {
+            int newHp = Math.max(0, currentHp - healAmount);
+            this.setHp(newHp);
+            return -healAmount;
+        }
+        int newHp = Math.min(maxHp, currentHp + healAmount);
+        this.setHp(newHp);
+        return healAmount;
+    }
 }
